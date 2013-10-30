254b54ce214075c17a25daa1f3c0d13608e947d6
Michał Stocki <mstocki@ydp.eu>
2013-10-30 10:48:21 +0100

    'recorder.js' split into two files. From now on 'recorder.js' is independent JavaScript API for recorder.swf. Updated demo.

--- html/js/recorder.js
+++ html/js/recorder.js
@@ -1,125 +1,8 @@
 
+(function(global) {
+  var Recorder;
 
-
-function microphone_recorder_events()
-{
-  $('#status').text("Microphone recorder event: " + arguments[0]);
-  var name, $controls;
-  switch(arguments[0]) {
-  case "ready":
-    var width = parseInt(arguments[1]);
-    var height = parseInt(arguments[2]);
-    Recorder.uploadFormId = "#uploadForm";
-    Recorder.uploadFieldName = "upload_file[filename]";
-    Recorder.connect("recorderApp", 0);
-    Recorder.recorderOriginalWidth = width;
-    Recorder.recorderOriginalHeight = height;
-    $('.save_button').css({'width': width, 'height': height});
-  break;
-
-  case "no_microphone_found":
-    break;
-
-  case "microphone_user_request":
-    Recorder.showPermissionWindow();
-    break;
-
-  case "microphone_connected":
-    var mic = arguments[1];
-    Recorder.defaultSize();
-    Recorder.isReady = true;
-    if(configureMicrophone) {
-      configureMicrophone();
-    }
-    $('#upload_status').css({'color': '#000'}).text("Microphone: " + mic.name);
-    break;
-
-  case "microphone_not_connected":
-    Recorder.defaultSize();
-    break;
-
-  case "microphone_activity":
-    $('#activity_level').text(arguments[1]);
-    break;
-
-  case "recording":
-    name = arguments[1];
-    $controls = controlsEl(name);
-    Recorder.hide();
-    $controls.find('.record_button img').attr('src', 'images/stop.png');
-    $controls.find('.play_button').hide();
-    break;
-
-  case "recording_stopped":
-    name = arguments[1];
-    $controls = controlsEl(name);
-    var duration = arguments[2];
-    Recorder.show();
-    $controls.find('.record_button img').attr('src', 'images/record.png');
-    $('#duration').text(duration.toFixed(4) + " seconds");
-    $controls.find('.play_button').show();
-    break;
-
-  case "playing":
-    name = arguments[1];
-    $controls = controlsEl(name);
-    $controls.find('.record_button img').attr('src', 'images/record.png');
-    $controls.find('.play_button img').attr('src', 'images/stop.png');
-    $controls.find('.pause_button').show();
-    break;
-
-  case "playback_started":
-    name = arguments[1];
-    var latency = arguments[2];
-    break;
-
-  case "stopped":
-    name = arguments[1];
-    $controls = controlsEl(name);
-    $controls.find('.record_button img').attr('src', 'images/record.png');
-    $controls.find('.play_button img').attr('src', 'images/play.png');
-    $controls.find('.pause_button').hide();
-    break;
-
-  case "save_pressed":
-    Recorder.updateForm();
-    break;
-
-  case "saving":
-    name = arguments[1];
-    break;
-
-  case "saved":
-    name = arguments[1];
-    var data = $.parseJSON(arguments[2]);
-    if(data.saved) {
-      $('#upload_status').css({'color': '#0F0'}).text(name + " was saved");
-    } else {
-      $('#upload_status').css({'color': '#F00'}).text(name + " was not saved");
-    }
-    break;
-
-  case "save_failed":
-    name = arguments[1];
-    var errorMessage = arguments[2];
-    $('#upload_status').css({'color': '#F00'}).text(name + " failed: " + errorMessage);
-    break;
-
-  case "save_progress":
-    name = arguments[1];
-    var bytesLoaded = arguments[2];
-    var bytesTotal = arguments[3];
-    $('#upload_status').css({'color': '#000'}).text(name + " progress: " + bytesLoaded + " / " + bytesTotal);
-    break;
-  }
-}
-
-function controlsEl(name) {
-  return $('.control_panel.'+name);
-}
-
-(function() {
-  window.Recorder = {
+  Recorder = {
     recorder: null,
     recorderOriginalWidth: 0,
     recorderOriginalHeight: 0,
@@ -247,4 +130,8 @@ function controlsEl(name) {
       Recorder.recorder.setLoopBack(val);
     }
   };
-})();
+
+  global.FWRecorder = Recorder;
+
+
+})(this);
